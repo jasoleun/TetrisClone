3fe62aeea272a7524e26c77f435a71c145c3591f
Jason Leung <Jasoleun@gmail.com>
2021-02-25 16:44:11 -0800

    Added rotation

--- src/Playfield.cs
+++ src/Playfield.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 
 namespace TetrisClone
 {
     class Playfield
     {
         private int[,] array = new int[10, 40];
-        private int[,] tempArray = new int[10, 40];
-        private double elapsed;
 
-        private Vector2 currentTetraminoPos = new Vector2(0, 0);
-        private int[,] currentTetramino;
+        private BlockRotator blockRotator = new BlockRotator();
+
         private BlockType blockType = new BlockType();
         private Texture2D blockTexture;
         private Texture2D playfieldTexture;
-        private Vector2 playfieldPosition = new Vector2(0, 0);
-        private Vector2 blockPosition;
+
+        private KeyboardState oldState;
 
         private Color[] tetraminoColors =
         {
@@ -32,6 +30,8 @@ namespace TetrisClone
             Color.Red,
         };
 
+        private int[,] currentTetramino;
+
         public Playfield()
         {
 
@@ -41,57 +41,45 @@ namespace TetrisClone
         {
             blockTexture = Content.Load<Texture2D>("blockTexture");
             playfieldTexture = Content.Load<Texture2D>("playfieldTexture");
-            currentTetramino = blockType.IBlock;
+
+            currentTetramino = blockType.ZBlock;
         }
 
         public void Update(GameTime gameTime)
         {
-            elapsed += gameTime.ElapsedGameTime.TotalMilliseconds;
-
-            if (Keyboard.GetState().IsKeyDown(Keys.A) && currentTetraminoPos.X > 0)
+            KeyboardState newState = Keyboard.GetState();
+            if (oldState.IsKeyUp(Keys.A) && newState.IsKeyDown(Keys.A))
             {
-                currentTetraminoPos.X -= 1;
+                currentTetramino = blockRotator.Rotate(currentTetramino, "left");
             }
-
-            else if (Keyboard.GetState().IsKeyDown(Keys.D) && currentTetraminoPos.X < 6)
+            else if (oldState.IsKeyUp(Keys.D) && newState.IsKeyDown(Keys.D))
             {
-                currentTetraminoPos.X += 1;
+                currentTetramino = blockRotator.Rotate(currentTetramino, "right");
             }
-
-            for (int i = 0; i < currentTetramino.GetLength(0); i++)
-            {
-                for (int j = 0; j < currentTetramino.GetLength(1); j++)
-                {
-                    tempArray[i + (int)currentTetraminoPos.X, j + (int)currentTetraminoPos.Y] = currentTetramino[i, j];
-                }
-            }
-
-            if (elapsed >= 1000)
-            {
-                if (currentTetraminoPos.Y <= 5)
-                {
-                    currentTetraminoPos.Y += 1;
-                }
-                Array.Clear(tempArray, 0, tempArray.Length);
-                elapsed = 0;
-            }
+            oldState = newState;
         }
 
         public void Draw(SpriteBatch _spriteBatch)
         {
             _spriteBatch.Begin();
-            _spriteBatch.Draw(playfieldTexture, playfieldPosition, Color.White);
-            for (int i = 0; i < tempArray.GetLength(0); i++)
+            _spriteBatch.Draw(playfieldTexture, new Vector2(0, 0), Color.White);
+            for (int row = 0; row < array.GetLength(0); row++)
             {
-                for (int j = 0; j < tempArray.GetLength(1); j++)
+                for (int col = 0; col < array.GetLength(1); col++)
                 {
-                    blockPosition.X = i * 16;
-                    blockPosition.Y = j * 16;
-                    if (tempArray[i, j] != 0)
+                    if (array[row, col] != 0)
                     {
-                        int currentBlock = tempArray[i, j];
-                        Color currentBlockColor = tetraminoColors[currentBlock];
-                        _spriteBatch.Draw(blockTexture, blockPosition, currentBlockColor);
+                        _spriteBatch.Draw(blockTexture, new Vector2(row * 16, col * 16), tetraminoColors[array[row, col]]);
+                    }
+                }
+            }
+            for (int row = 0; row < currentTetramino.GetLength(0); row++)
+            {
+                for (int col = 0; col < currentTetramino.GetLength(1); col++)
+                {
+                    if (currentTetramino[row, col] != 0)
+                    {
+                        _spriteBatch.Draw(blockTexture, new Vector2(col * 16, row * 16), tetraminoColors[currentTetramino[row, col]]);
                     }
                 }
             }
